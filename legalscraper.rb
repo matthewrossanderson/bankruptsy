862e41b691c158058e18fa79695ce7f21a40da1c
Matt Anderson <matthewrossanderson@gmail.com>
2012-06-21 15:56:17 -0400

    Create Scraper Class
    Scraper accesses site, parses html, extracts casename and url and puts
    them into an array of hashes.

--- legalscraper.rb
+++ legalscraper.rb
@@ -1,49 +1,60 @@
-require 'httparty'
-require 'pp'
-require 'nokogiri'
-require 'rubygems'
-require 'mechanize'
+class Scrape
 
-class DBProxy
-	include HTTParty
-	http_proxy 'surf-proxy.intranet.db.com', 8080
-end
+	require 'httparty'
+	require 'pp'
+	require 'nokogiri'
+	require 'rubygems'
+	require 'mechanize'
 
-baseuri = 'http://www.kccllc.net/default.asp'
-options = {
-	:ddlSort => 'RecentFile',
-	:hfLastSort=>'CaseName',
-	:hfCurrPage=>0
-}
+	class DBProxy
+		include HTTParty
+		http_proxy 'surf-proxy.intranet.db.com', 8080
+	end
+	
+	def currentcases
+		baseuri = 'http://www.kccllc.net/default.asp'
+		options = {
+			:ddlSort => 'RecentFile',
+			:hfLastSort=>'CaseName',
+			:hfCurrPage=>0
+		}
 
-response = DBProxy.post( baseuri, :body => options )
-doc = Nokogiri.HTML(response)
-currentcases = Array.new
+		response = DBProxy.post( baseuri, :body => options )
+		doc = Nokogiri.HTML(response)
+		cases = Array.new
 
-doc.css('#divContent table tr td table').each do |table|
-	if /tblCase\d/.match(table['id'])
-		table.css('tr td a').each do |a|
-			if /http:\/\/.*/.match(a['href'])
-				link = a['href']
-			else 
-				link = 'http://www.kccllc.net/' + a['href']
+		doc.css('#divContent table tr td table').each do |table|
+			if /tblCase\d/.match(table['id'])
+				table.css('tr td a').each do |a|
+					if /http:\/\/.*/.match(a['href'])
+						link = a['href']
+					else 
+						link = 'http://www.kccllc.net/' + a['href']
+					end
+					#/[0-9]*\-[0-9]*\s\|\s/  Regex for matching the casenumber
+					cases.push Hash[:casename => a.text, :url => link]
+				end
 			end
-			puts link
-			currentcases.push a.text + " | " + link +  "\n"
+		end
+		cases
+		#cases.each { |c| puts c[:url] + "  "  + c[:casename] }
+		#File.open('output.csv', 'w') {|f| f.write(currentcases.join) }
+	end
+	
+	def case(url)
+	
+		if /http:\/\/www\.kccllc\.net\/\S*/.match(url)
+			agent = Mechanize.new do |a|
+				a.set_proxy('surf-proxy.intranet.db.com', 8080)
+				a.user_agent_alias = "Windows IE 8"
+			end
+			page = agent.get(url)
+			continueform = page.form('frmDisclaimer')
+			page = agent.submit(continueform)
+			page.body
+		else
+			"External website: <a href='#{url}'>#{url}</a>"
 		end
 	end
 end
-
-#pp currentcases.join
-#File.open('output.csv', 'w') {|f| f.write(currentcases.join) }
-
-agent = Mechanize.new do |a|
-  a.set_proxy('surf-proxy.intranet.db.com', 8080)
-  a.user_agent_alias = "Windows IE 8"
-end
-
-page = agent.get('http://www.kccllc.net/afa')
-continueform = page.form('frmDisclaimer')
-page = agent.submit(continueform)
-page.body
 
