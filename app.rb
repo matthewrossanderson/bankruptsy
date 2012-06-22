1ba98feab4ea00f89a06487b13cb90ba4a7d24fe
Matt Anderson <matthewrossanderson@gmail.com>
2012-06-22 15:35:25 -0400

    Getting closer to full feed validation

--- app.rb
+++ app.rb
@@ -20,18 +20,20 @@ get '/rss', :provides => ['rss', 'atom', 'xml'] do
 	cache_control :public, :must_revalidate, :max_age => 60
 	scraper = Scrape.new
 	cases = scraper.currentcases	
-	content_type 'application/rss+xml'
+	#content_type 'application/rss+xml'
 	
 	@rss = RSS::Maker.make("atom") do |maker|
-	  maker.channel.author = "Author"
+	  maker.channel.author = "manderson"
 	  maker.channel.updated = Time.now.to_s
-	  maker.channel.about = "bankruptsy"
-	  maker.channel.title = "Example Feed"
+	  maker.channel.about = "http://electric-frost-5456.herokuapp.com/rss"
+	  maker.channel.title = "Bankruptsy Feed"
 	  cases.each do |c|
 		  maker.items.new_item do |item|
+			item.id = c[:casename]
 			item.link = c[:url]
 			item.title = c[:casename]
 			item.updated = Time.now.to_s
+			item.description = c[:casename]
 		  end
 		end
 	end
